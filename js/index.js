07af5e92279a5406bfa2c0c7c12dd69700935be7
Guillaume Naval <navalguillaume@gmail.com>
2021-06-05 14:28:21 +0200

    appel API + home

--- js/index.js
+++ js/index.js
@@ -1,39 +1,77 @@
 main()
 
-
 // FONCTION MAIN DE LA PAGE
 
 async function main() {
     const products = await getProducts()
     console.log(products)
 
+    // Product card
+
+    const mainContent = document.getElementById("main-home");
+
     for (product of products) {
-        displayProducts(product)
+
+        //Page du produit
+        productPageLink = document.createElement("a");
+        mainContent.appendChild(productPageLink);
+        urlPage = "product.html?id=" + product._id;
+        productPageLink.setAttribute('href', urlPage);
+        productPageLink.setAttribute('class', "card__link");
+
+        // Product Card
+        let cardProduct = document.createElement("div");
+        productPageLink.appendChild(cardProduct);
+        cardProduct.setAttribute("class", "product");
+
+        // Product name
+        let nameProduct = document.createElement("h2");
+        cardProduct.appendChild(nameProduct);
+        nameProduct.textContent = product.name;
+        nameProduct.setAttribute("class", "product__name");
+
+        // Product image
+        let imageProduct = document.createElement("img");
+        cardProduct.appendChild(imageProduct);
+        imageProduct.src = product.imageUrl;
+        imageProduct.setAttribute("class", "product__img");
+
+        // Product description
+        let descriptionProduct = document.createElement("p");
+        cardProduct.appendChild(descriptionProduct);
+        descriptionProduct.textContent = product.description;
+        descriptionProduct.setAttribute("class", "product__description");
+
+        //Product du prix
+        let priceProduct = document.createElement("p");
+        cardProduct.appendChild(priceProduct);
+        priceProduct.setAttribute("class", "price");
+        priceProduct.textContent = product.price + ' $';
     }
 }
 
-
-// FETCH API
+// FETCH
 
 function getProducts() {
     return fetch("http://localhost:3000/api/cameras")
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (products) {
-            return products
+        .then(function (res) {
+            return res.json()
         })
         .catch(function (error) {
             alert(error)
         })
 }
 
-// AFFICHAGE DES PRODUITS
+/*  AFFICHAGE DES PRODUITS
 
-function displayProducts() {
+function displayProducts(product) {
     document.getElementById("main").innerHTML += `
     <article class="product">
-        <h2 id="product__name" class="product_name" >${product.name}</h2>
-        <p id="product__description" class="product_description">${product.description}</p>
+        <h2 class="product__name">${product.name}</h2>
+        <img class="product__img" src="${product.imageUrl}">
+        <p class="product__description">${product.description}</p>
     </article>`
 }
+*/
+
+
